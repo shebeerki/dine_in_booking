0d9323cbb9f2612272e63182520b0ea81fec18a0
shebeerki <shebeer.i.k@gmail.com>
2025-11-05 23:40:52 +0530

    initial test for list in jsx format

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,15 @@
+function ListGroup(){
+    return (
+        <>
+            <h1>List Group</h1>
+            <ul className="grid">
+                <li key="list1">list 1</li>
+                <li key="list2">list 2</li>
+                <li key="list3">list 3</li>
+                <li key="list4">list 4</li>
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
